d8c73eb473802a4b7ee1741df694a6a3496cf460
Panagiotis Roubatsis <proubatsis@gmail.com>
2016-12-28 10:27:19 -0500

    First commit

---
+++ todo-app/README.md
@@ -0,0 +1,12 @@
+# Todo App #
+
+## Build & Run ##
+
+```sh
+$ cd Todo_App
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ todo-app/src/main/scala/com/github/proubatsis/todo/TodoServlet.scala
@@ -0,0 +1,16 @@
+package com.github.proubatsis.todo
+
+import org.scalatra._
+
+class TodoServlet extends TodoAppStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
